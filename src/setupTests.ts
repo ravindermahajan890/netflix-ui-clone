0f087f5bb7849717436d7a9a8f5bb1b87b9da171
GokulnathP <4gokul2000@gmail.com>
2023-03-16 09:50:31 +0530

    feat: [TMSF-19] - Add environment variable configuration

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,2 +1,8 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 import '@testing-library/jest-dom';
+
+jest.mock('./env', () => ({
+  default: {
+    VITE_APP_NAME: 'Test App Name',
+  },
+}));
